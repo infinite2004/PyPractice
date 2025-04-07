8f3139c18a8e2c70cf2c67a6b342bd9b288c7576
infinite2004 <qureshi.abdul.227@gmail.com>
2025-04-07 09:03:50 -0400

    update

---
+++ month1/week1/day1.py
@@ -0,0 +1,8 @@
+print("Hello world")
+
+
+print("5 +3 =", 5+3)
+print("10 /2=" , 10/2)
+print("2 ** 3 =",  2**3)
+print("5 -3 =" , 5-3)
+print("10 // 4", 10//4)
---
+++ month1/week1/day2.py
@@ -0,0 +1,6 @@
+name = input("What is your name")
+age  =  int(input(" How old are you"))
+year_100 = 2025 + (100 - age)
+print(f"Hey {name} , you'll turn 100 years old n the year {year_100}")
+
+
