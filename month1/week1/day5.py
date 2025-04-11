d1ddd774efd7694a4cc363f987b8a9561531293a
infinite2004 <qureshi.abdul.227@gmail.com>
2025-04-11 12:52:18 -0400

    day5

--- month1/week1/day5.py
+++ month1/week1/day5.py
@@ -1,1 +1,45 @@
-#  day 5 code 
+#  While Loops
+
+x = 0 
+while x < 5:
+	print("Counting:" , x)
+	x +=1
+
+y = 0
+while y < 10:
+	print("Counting:" ,y)
+	y += 2
+
+
+# Count Down
+
+print("Count down")
+
+count = 10
+while count > 0:
+	print(count)
+	count -=1
+print("Take off")
+
+timer = 30
+while timer > 10:
+	print(timer, "wake up")
+	timer -=1
+	if timer == 15:
+		print(timer , "go back to sleep")
+		timer -=1
+print("ahhh")
+
+
+# for LOOPS
+
+for letter in "hello":
+	print(letter)
+
+
+
+birthday  = input("what is your name")
+
+for letter in birthday:
+	print(letter)
+
